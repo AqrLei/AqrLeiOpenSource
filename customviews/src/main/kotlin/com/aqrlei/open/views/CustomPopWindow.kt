da3cd1789ea94b774aa43ea3fedd524468a140cd
AqrLei <aqrdeveloper@gmail.com>
2018-10-15 09:56:08 +0800

    add new View $ Util

---
+++ customviews/src/main/kotlin/com/aqrlei/open/views/CustomPopWindow.kt
@@ -0,0 +1,211 @@
+package com.aqrlei.open.views
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.annotation.LayoutRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_custom_pop.view.*
+import kotlinx.android.synthetic.main.layout_custom_pop_item.view.*
+
+
+/**
+ * @author aqrlei on 2018/10/10
+ */
+class CustomPopWindow private constructor(
+        context: Context,
+        private val popWindowConfigure: PopConfigure,
+        private val popCorner: PopCorner,
+        private val itemAction: ((Int, PopContent, PopupWindow) -> Unit)? = null) {
+    private val inflater = LayoutInflater.from(context)
+    private val rootView = inflater.inflate(R.layout.layout_custom_pop, null)
+    private val data = ArrayList<PopContent>()
+    private val popupWindow = PopupWindow(context)
+    private val mAdapter = CustomPopRecyclerAdapter(R.layout.layout_custom_pop_item)
+
+    init {
+        rootView.outsideCl.setOnClickListener { popupWindow.dismiss() }
+        with(rootView.contentRv) {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            adapter = mAdapter
+        }
+        rootView.contentRv.adapter = mAdapter
+
+        (rootView.contentRv.layoutParams as? ConstraintLayout.LayoutParams)?.apply {
+            rightMargin = DensityUtil.dip2px(popWindowConfigure.marginRight)
+        }
+        when (popCorner) {
+            PopCorner.TOP -> {
+                with(rootView.topIv) {
+                    visibility = View.VISIBLE
+                    rootView.additionalTopV.visibility = View.VISIBLE
+                    setImageResource(R.mipmap.ic_arrow_top)
+                }
+            }
+            PopCorner.BOTH_NOT_SHOW -> {
+                rootView.bottomIv.visibility = View.GONE
+                rootView.topIv.visibility = View.GONE
+                rootView.additionalTopV.visibility = View.GONE
+                rootView.additionalBottomV.visibility = View.GONE
+            }
+            PopCorner.BOTTOM -> {
+                with(rootView.bottomIv) {
+                    visibility = View.VISIBLE
+                    rootView.additionalBottomV.visibility = View.VISIBLE
+                    setImageResource(R.mipmap.ic_arrow_bottom)
+                }
+            }
+        }
+        with(popupWindow) {
+            width = LinearLayout.LayoutParams.MATCH_PARENT
+            height = LinearLayout.LayoutParams.MATCH_PARENT
+            isOutsideTouchable = true
+            isFocusable = true
+            contentView = rootView
+            setBackgroundDrawable(ColorDrawable(Color.parseColor("#00000000")))
+        }
+    }
+
+    inner class CustomPopRecyclerAdapter(
+            @LayoutRes private val resId: Int) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view = inflater.inflate(resId, parent, false)
+            popWindowConfigure.run {
+                val paddingLeft = DensityUtil.dip2px(itemPaddingLeft)
+                val paddingTop = DensityUtil.dip2px(itemPaddingTop)
+                val paddingRight = DensityUtil.dip2px(itemPaddingRight)
+                val paddingBottom = DensityUtil.dip2px(itemPaddingBottom)
+                val drawablePadding = DensityUtil.dip2px(itemDrawablePadding)
+                view.popContentTv.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom)
+                view.popContentTv.compoundDrawablePadding = drawablePadding
+            }
+            return CustomPopRecyclerViewHolder(view)
+        }
+
+        override fun getItemCount(): Int = data.size
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener {
+                itemAction?.invoke(position, data[position], popupWindow)
+            }
+            holder.itemView.popContentTv.apply {
+                setTextSize(TypedValue.COMPLEX_UNIT_DIP, popWindowConfigure.itemTextSize)
+                text = data[position].subtitle
+
+                val length = popWindowConfigure.run {
+                    DensityUtil.dip2px(itemTextSize + itemPaddingBottom + itemPaddingTop)
+                }
+
+                val drawable = data[position].icon?.apply {
+                    setBounds(0, 0, length, length)
+                }
+                setCompoundDrawables(drawable, null, null, null)
+            }
+        }
+    }
+
+
+    fun dismiss() {
+        popupWindow.dismiss()
+    }
+
+    fun show(data: List<PopContent>, anchor: View, noArrowGravity: Int = Gravity.TOP) {
+        this.data.clear()
+        this.data.addAll(data)
+        mAdapter.notifyDataSetChanged()
+        when (popCorner) {
+            PopCorner.BOTH_NOT_SHOW -> {
+                setShowGravity(noArrowGravity, anchor.height)
+            }
+            PopCorner.BOTTOM -> {
+                setShowGravity(Gravity.BOTTOM, anchor.height)
+            }
+            PopCorner.TOP -> {
+                setShowGravity(Gravity.TOP, anchor.height)
+            }
+        }
+        popupWindow.showAtLocation(rootView, Gravity.NO_GRAVITY, 0, 0)
+    }
+
+    private fun setShowGravity(gravity: Int, anchorHeight: Int) {
+        val topLp = rootView.topFl.layoutParams as? ConstraintLayout.LayoutParams
+        val bothRightMargin = DensityUtil.dip2px(popWindowConfigure.arrowMarginRight)
+        topLp?.apply {
+            verticalBias = if (gravity == Gravity.TOP) 0.0F else 1.0F
+            rightMargin = bothRightMargin
+        }
+        val bottomLp = rootView.bottomFl.layoutParams as? ConstraintLayout.LayoutParams
+        bottomLp?.apply {
+            rightMargin = bothRightMargin
+        }
+        if (gravity == Gravity.TOP) {
+            topLp?.apply {
+                topMargin = DensityUtil.dip2px(popWindowConfigure.marginTop) + anchorHeight
+            }
+        } else {
+            bottomLp?.apply {
+                bottomMargin = DensityUtil.dip2px(popWindowConfigure.marginTop) + anchorHeight
+            }
+        }
+
+        rootView.topFl.layoutParams = topLp
+        rootView.bottomFl.layoutParams = bottomLp
+    }
+
+    inner class CustomPopRecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    data class PopContent(
+            var icon: Drawable? = null,
+            var subtitle: String
+    )
+
+    data class PopConfigure(
+            var arrowMarginRight: Float = 0F,
+            var marginTop: Float = 0F,
+            var marginRight: Float = 0F,
+            var itemPaddingLeft: Float = 0F,
+            var itemPaddingTop: Float = 0F,
+            var itemPaddingRight: Float = 0F,
+            var itemPaddingBottom: Float = 0F,
+            var itemDrawablePadding: Float = 0F,
+            var itemTextSize: Float = 0F,
+            var backgroundColor: String = "")
+
+    enum class PopCorner {
+        TOP, BOTTOM, BOTH_NOT_SHOW
+    }
+
+    object Builder {
+        private var itemAction: ((Int, PopContent, PopupWindow) -> Unit)? = null
+        private var popCorner: PopCorner = PopCorner.BOTH_NOT_SHOW
+        private var popConfigure = PopConfigure()
+        fun addPopWindowConfigure(configure: PopConfigure): Builder {
+            popConfigure = configure
+            return this
+        }
+
+        fun setPopCorner(popCorner: PopCorner): Builder {
+            this.popCorner = popCorner
+            return this
+        }
+
+        fun addItemAction(action: (Int, PopContent, PopupWindow) -> Unit): Builder {
+            itemAction = action
+            return this
+        }
+
+        fun builder(context: Context): CustomPopWindow {
+            return CustomPopWindow(context, popConfigure, popCorner, itemAction)
+        }
+    }
+}
+
---
+++ customviews/src/main/kotlin/com/aqrlei/open/views/RoundBar.kt
@@ -0,0 +1,271 @@
+package com.aqrlei.open.views
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.SweepGradient
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.AttributeSet
+import android.view.View
+
+
+/**
+ * @author  aqrLei on 2018/4/25
+ */
+
+/**
+ * @param context 上下文关系
+ * @param attrs 属性
+ * @param defStyleAttr 默认属性
+ * @param defStyleRes 默认资源ID
+ * @description: 画一个圆形或半圆的简单进度条
+ * */
+
+@Suppress("unused")
+class RoundBar @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null) :
+        View(context, attrs) {
+    /**
+     * 标明画的时候是当前进度开始还是从头开始
+     * */
+    companion object {
+
+        /**异常销毁需要保存的数据的key*/
+        private const val INSTANCE = "instance"
+        private const val INSTANCE_BOOLEAN = "boolean"
+        private const val INSTANCE_DEGREE = "degree"
+
+        private const val DEFAULT_BG_COLOR = "#bbbbbb"
+        private const val DEFAULT_PROGRESS_COLOR = "#41a9f8"
+    }
+
+    enum class CapStyle {
+        BUTT, ROUND, SQUARE
+    }
+
+    private val mPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)//画笔
+    private var mBackgroundColor: Int = 0//背景颜色
+    private var mProgressColor: Int = 0//进度颜色
+    private var mGradientListColor: ArrayList<Int>? = null//渐变色
+    private var mRadius: Float = 100F//圆半径
+    private var mMaxProgress: Float = 10F//最大的进度
+    private var mCurrentProgress: Float = 5f//当前进度
+    private var mIsOpenAnimation: Boolean = false//是否开启动画
+    private var mIsUseGradientColor: Boolean = false//是否使用渐变
+    private var mStartDegree: Float = 0F// 画圆开始的角度
+    private var mSweepDegree: Float = 0F//画圆扫过的角度
+    private var mRotateDegree: Float = 0F// 画布旋转的角度
+    private var mProgressDegree: Float = 0F//当前进度该画的角度
+    private var mDrawDegree: Float = 0F//画的起始角度
+    private var mAnimationV: Float = 0F//画的速度
+    private var mChangeListener: OnDrawProgressChangeListener? = null//当前进度监听器
+
+
+    init {
+        mDrawDegree = 0F
+        mGradientListColor = ArrayList()
+        mGradientListColor?.add(Color.parseColor("#21ADF1"))
+        mGradientListColor?.add(Color.parseColor("#2287EE"))
+        mPaint.style = Paint.Style.STROKE
+        /**
+         * val typedArray = TintTypedArray.obtainStyledAttributes(
+        mContext,
+        attrs,
+        R.styleable.RoundBar)*/
+        context.obtainStyledAttributes(attrs, R.styleable.RoundBar)?.run {
+            setBackgroundColor(getColor(
+                    R.styleable.RoundBar_backgroundColor,
+                    Color.parseColor(DEFAULT_BG_COLOR)))
+            setProgressColor(
+                    getColor(R.styleable.RoundBar_progressColor,
+                            Color.parseColor(DEFAULT_PROGRESS_COLOR)))
+            setRadius(getFloat(R.styleable.RoundBar_radius, mRadius))
+            mMaxProgress = getFloat(R.styleable.RoundBar_maxProgress, mMaxProgress)
+            mCurrentProgress = getFloat(R.styleable.RoundBar_progress, mCurrentProgress)
+            setOpenAnimation(getBoolean(R.styleable.RoundBar_openAnimation, false))
+            setUseGradientColor(getBoolean(R.styleable.RoundBar_useGradientColor, false))
+            setRoundWidth(getFloat(R.styleable.RoundBar_roundWidth, 10f))
+
+            setCapStyle(CapStyle.values()[getInteger(R.styleable.RoundBar_capStyle, CapStyle.BUTT.ordinal)])
+            setStartDegree(getFloat(R.styleable.RoundBar_startDegree, 0f))
+            setSweepDegree(getFloat(R.styleable.RoundBar_sweepDegree, 360f))
+            setRotateDegree(getFloat(R.styleable.RoundBar_rotateDegree, 0f))
+            setAnimationV(getFloat(R.styleable.RoundBar_animationVelocity, 1f))
+            recycle()
+
+        }
+    }
+
+    fun setAnimationV(v: Float) {
+        mAnimationV = v
+    }
+
+    fun setStartDegree(degree: Float) {
+        mStartDegree = degree
+    }
+
+    fun setSweepDegree(degree: Float) {
+        mSweepDegree = degree
+    }
+
+    fun setRotateDegree(degree: Float) {
+        mRotateDegree = degree
+    }
+
+    fun setCapStyle(capStyle: CapStyle) {
+        when (capStyle) {
+            CapStyle.BUTT -> mPaint.strokeCap = Paint.Cap.BUTT
+            CapStyle.ROUND -> mPaint.strokeCap = Paint.Cap.ROUND
+            CapStyle.SQUARE -> mPaint.strokeCap = Paint.Cap.SQUARE
+        }
+    }
+
+    fun setRoundWidth(width: Float) {
+        mPaint.strokeWidth = width
+    }
+
+    fun setRadius(radius: Float) {
+        mRadius = radius
+    }
+
+    fun setGradientColor(gradientColor: ArrayList<Int>?) {
+        if (gradientColor != null && gradientColor.size < 2) {
+            throw IllegalArgumentException("needs >= 2 number of colors")
+        }
+        mGradientListColor = gradientColor
+    }
+
+    fun getGradientColor(): ArrayList<Int>? {
+        return mGradientListColor
+    }
+
+    @Synchronized
+    fun setProgress(progress: Float) {
+        mCurrentProgress = progress
+        mProgressDegree = (mSweepDegree * (mCurrentProgress / mMaxProgress)).toInt().toFloat()
+        mProgressDegree = if (mProgressDegree > mSweepDegree) mSweepDegree else mProgressDegree
+        mDrawDegree = 0F
+        postInvalidate()
+    }
+
+    @Synchronized
+    fun setMaxProgress(maxProgress: Float) {
+        mMaxProgress = maxProgress
+        setProgress(mCurrentProgress)
+    }
+
+    @Synchronized
+    fun getProgress(): Float {
+        return mCurrentProgress
+    }
+
+    @Synchronized
+    fun getMaxProgress(): Float {
+        return mMaxProgress
+    }
+
+    fun setProgressColor(color: Int) {
+        mProgressColor = color
+    }
+
+    override fun setBackgroundColor(color: Int) {
+        mBackgroundColor = color
+    }
+
+    fun setUseGradientColor(useGradientColor: Boolean) {
+        mIsUseGradientColor = useGradientColor
+    }
+
+    fun setOpenAnimation(openAnimation: Boolean) {
+        mIsOpenAnimation = openAnimation
+    }
+
+    fun setOnDrawProgressListener(changeListener: OnDrawProgressChangeListener) {
+        mChangeListener = changeListener
+    }
+
+    @SuppressLint("DrawAllocation", "NewApi")
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val width = measuredWidth
+        val height = measuredHeight
+        val centerX = (width / 2).toFloat()
+        val centerY = (height / 2).toFloat()
+        val left = centerX - mRadius
+        val top = centerY - mRadius
+        val right = centerX + mRadius
+        val bottom = centerY + mRadius
+        val rStartDegree = mStartDegree - mRotateDegree
+        canvas?.save()
+        canvas?.rotate(mRotateDegree, centerX, centerY)
+        mPaint.shader = null
+        mPaint.color = mBackgroundColor
+        canvas?.drawArc(left, top, right, bottom, rStartDegree,
+                mSweepDegree, false, mPaint)
+        if (mIsUseGradientColor) {
+            mGradientListColor?.let {
+                mPaint.shader = SweepGradient(centerX, centerY, it[0], it[1])
+            }
+
+        } else {
+            mPaint.color = mProgressColor
+        }
+        if (mIsOpenAnimation) {
+            mChangeListener?.onDrawProgressChange(
+                    if (mDrawDegree >= mProgressDegree) mCurrentProgress
+                    else (mCurrentProgress * mDrawDegree / mProgressDegree)
+            )
+            canvas?.drawArc(left, top, right, bottom, mStartDegree, mDrawDegree, false, mPaint)
+            if (mDrawDegree < mProgressDegree) {
+                mDrawDegree += mAnimationV
+                mDrawDegree = Math.min(mDrawDegree, mProgressDegree)
+                invalidate()
+            }
+        } else {
+            canvas?.drawArc(left, top, right, bottom, mStartDegree, mProgressDegree, false, mPaint)
+        }
+        canvas?.restore()
+    }
+
+    override fun onSaveInstanceState(): Parcelable {
+        val bundle = Bundle()
+        bundle.putParcelable(INSTANCE, super.onSaveInstanceState())
+        val bool = booleanArrayOf(mIsOpenAnimation, mIsUseGradientColor)
+        val degrees = floatArrayOf(mStartDegree, mSweepDegree, mProgressDegree, mRotateDegree)
+
+        with(bundle) {
+            putBooleanArray(INSTANCE_BOOLEAN, bool)
+            putFloatArray(INSTANCE_DEGREE, degrees)
+        }
+        return bundle
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        if (state is Bundle) {
+            with(state) {
+                val bool = getBooleanArray(INSTANCE_BOOLEAN)
+                val degrees = getFloatArray(INSTANCE_DEGREE)
+                mIsOpenAnimation = bool[0]
+                mIsUseGradientColor = bool[1]
+                mStartDegree = degrees[0]
+                mSweepDegree = degrees[1]
+                mProgressDegree = degrees[2]
+                mRotateDegree = degrees[3]
+                super.onRestoreInstanceState(getParcelable(INSTANCE))
+            }
+            return
+        }
+        super.onRestoreInstanceState(state)
+    }
+
+    /**
+     * 回调接口，传递 当前进度
+     * */
+    interface OnDrawProgressChangeListener {
+        fun onDrawProgressChange(currentProgress: Float)
+    }
+}
