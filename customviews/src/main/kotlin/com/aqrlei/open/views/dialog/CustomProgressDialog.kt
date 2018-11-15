428c3c287589edf8073edbff075530cb9d5d1eb7
AqrLei <aqrdeveloper@gmail.com>
2018-11-15 11:52:13 +0800

    add CustomProgressDialog TODO

---
+++ customviews/src/main/kotlin/com/aqrlei/open/views/dialog/CustomProgressDialog.kt
@@ -0,0 +1,10 @@
+package com.aqrlei.open.views.dialog
+
+import androidx.fragment.app.DialogFragment
+
+/**
+ * @author aqrlei on 2018/11/15
+ */
+class CustomProgressDialog : DialogFragment() {
+    //TODO   style ， layer-list , drawable,参考Android源码
+}
